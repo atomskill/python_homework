3e36248f0a7a5899a825718ae5889db299a9a710
ya <topSecret>
2023-08-01 00:16:48 +0500

    errors

--- client.py
+++ client.py
@@ -1,17 +1,20 @@
 from socket import create_connection
+
 class Client():
-    def __init__(self, host, port, timeout = 1):
-            try:
-                self.connect = create_connection((host, port))
-            except:
-                 pass
+    def __init__(self, host, port, timeout = None):
+        self.host = host
+        self.port = port
+        self.timeout = timeout
+    
     def put(self, key, value, timestamp):
-        self.connect.send(bytes(f"put {key} {value} {timestamp}\n",'utf-8'))
-        return self.connect.recv(1024)
+        connect = create_connection((self.host, self.port), 1)
+        connect.sendall( f"put {key} {value} {timestamp}\n".encode('utf-8'))
+        return connect.recv(1024)
     
     def get(self, key):
-        self.connect.send(bytes(f"get {key}\n",'utf-8'))
-        return self.connect.recv(1024)    
+        connect = create_connection((self.host, self.port), 1)
+        connect.sendall(f"get {key}\n".encode('utf-8'))
+        return connect.recv(1024)
 
 class ClientError():
     def __init__(self):
