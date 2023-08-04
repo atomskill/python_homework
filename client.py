d0555ab73db0d48f7c73a5aa8d2a7a38b686f8f7
unknown <you@example.com>
2023-08-04 16:56:57 +0500

    tests pass

--- client.py
+++ client.py
@@ -14,8 +14,23 @@ class Client():
     def get(self, key):
         connect = socket.create_connection((self.host, self.port), self.timeout)
         connect.sendall(f"get {key}\n".encode('utf-8'))
-        return connect.recv(1024)
+        answer = connect.recv(1024).decode()
+        ansObjs = answer.split('\n')
+        if ansObjs[0] != 'ok':
+            raise ClientError("Bad Request")
+        metrics = {}
+        for m in ansObjs:
+            if(m == 'ok' or m == ''): continue
+            ms = m.split()
+            key = ms[0]
+            timestamp = float(ms[1])
+            value = float(ms[2])
+            if key not in metrics:
+                metrics[key] = [(value, timestamp)]
+            else:
+                metrics[key].append((value, timestamp))
+        return metrics
 
-class ClientError():
-    def __init__(self):
-        pass
+class ClientError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
