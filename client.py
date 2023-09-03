da64f3318617fb8c6b65b6cf659c4f3875b831eb
ya <topSecret>
2023-09-03 19:06:57 +0500

    fix

--- client.py
+++ client.py
@@ -2,36 +2,38 @@
 Socket клиент для получения и отправки метрик
 """
 
-from typing import Union
+from typing import Optional
 
 import socket
 from time import time
 
 class Client():
-    def __init__(self, host: str, port: int, timeout: Union[int, None] = None) -> None:
-        """ Задаём значения host, port и необязательный параметр timeout для подключения к серверу """
-        self.host = host
-        self.port = port
+    """ Класс Client преднозначен для отправки и приёма метрик на стороне клиента
+        Для этого используются методы put и get
+    """
+
+    def __init__(self, host: str, port: int, timeout: Optional[int] = None) -> None:
+        """ Задаём кортеж server, включающий в себя host и port, и необязательный параметр timeout для подключения к серверу """
+        self.server = (host, port)
         self.timeout = timeout
     
-    def put(self, key: str, value: float, timestamp: Union[int, None]) -> None:
+    def put(self, key: str, value: float, timestamp: Optional[int]) -> None:
         """ Метод put отправляет на сервер имя клиента и его параметр через точку, 
             значение параметра и время, когда были полученны данные
             
             Если timestamp не задан, то к timestamp приравнивается текущее время системы в секундах 
         """
-        if timestamp == None:
-            timestamp = int(time())
+        timestamp = timestamp or int(time())
         
         """ Подключаемся к серверу и отправляем данные """
-        with socket.create_connection((self.host, self.port), self.timeout) as connect:
+        with socket.create_connection(self.server, self.timeout) as connect:
             connect.sendall( f"put {key} {value} {timestamp}\n".encode('utf-8'))
             answer = connect.recv(1024)
         
         """ Проверяем, что сервер отработал запрос корректно 
             Вызываем исключение если нет
         """
-        if answer.decode('utf-8').split('\n')[0] != 'ok':
+        if answer.decode('utf-8') != 'ok\n\n':
             raise ClientError("Bad Request")
     
     def get(self, key: str) -> dict[str, tuple[float, int]]:
@@ -41,21 +43,22 @@ class Client():
 
             Подключаемся к серверу и отправляем ключ (имя сервера, данные с которого хотим получить)
         """
-        with socket.create_connection((self.host, self.port), self.timeout) as connect:
+        with socket.create_connection(self.server, self.timeout) as connect:
             connect.sendall(f"get {key}\n".encode('utf-8'))
             answer = connect.recv(1024).decode('utf-8')
         
-        """Парсим ответ от сервера"""
-        ansObjs = answer.split('\n')
+        """ Парсим ответ от сервера
+            Убираем последнюю пустую строку
+        """
+        ansObjs = answer.splitlines()[0:-1]
 
         """Проверяем что сервер отработал корректно, иначе вызываем исключение"""
-        if ansObjs[0] != 'ok':
+        if ansObjs.pop(0) != 'ok':
             raise ClientError("Bad Request")
         
         """Создаём словарь и заполняем его данными, которые вернул нам сервер"""
         metrics = {}
         for m in ansObjs:
-            if(m == 'ok' or m == ''): continue
             ms = m.split()
             key = ms[0]
             timestamp = float(ms[1])
